13faba93e73017ab51cc16d3a5b6867b5081f652
Amy Humrich <amy.humrich@gmail.com>
2017-10-11 22:31:00 -0500

    updated error messages so more than one called if need be. user name and e-mail address remain in text boxes.

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    return render_template('index.html', user_name_error='', password_error='',
-            e_mail_error = '', user_name='', password='', e_mail = '')
+    return render_template('index.html', name_range_error='', name_space_error = '', pw_space_error='', pw_match_error='', pw_range_error='',
+            email_space_error = '', email_range_error='', email_sign_error='', user_name='', password='', e_mail = '')
 
 def has_spaces(user_entry):
     if ' ' in user_entry:
@@ -34,49 +34,46 @@ def validate_user():
     password = request.form['password']
     verify_pw = request.form['verify_pw']
     e_mail = request.form['e_mail']
-    user_name_error = ''
-    password_error = ''
-    e_mail_error = ''
-
+    name_space_error = ''
+    name_range_error = ''
+    pw_range_error = ''
+    pw_match_error = ''
+    pw_space_error = ''
+    email_space_error=''
+    email_range_error=''
+    email_sign_error=''
+    
     if has_spaces(user_name):
-        user_name_error = 'Spaces are not valid'
-        user_name = ''
+        name_space_error = 'Spaces are not valid'
+    if len(user_name) > 20 or len(user_name) < 3:
+        name_range_error = 'User name out of range (3-20)'
         
-    else:
-        if len(user_name) > 20 or len(user_name) < 3:
-            user_name_error = 'User name out of range (3-20)'
-            user_name = ''
-            
+       
+    if len(password) > 20 or len(password) < 3:
+        pw_range_error = 'Password out of range(3-20)'
+    if password != verify_pw:
+        pw_match_error = 'Passwords do not match'
     if has_spaces(password):
-        password_error = 'Spaces are not valid'
-        password = ''
-        verify_pw = ''
-    elif len(password) > 20 or len(password) < 3:
-        password_error = 'Password out of range(3-20)'
-        password = ''
-        verify_pw = ''
-    else:
-        if password != verify_pw:
-            password_error = 'Passwords do not match'
-            password = ''
-            verify_pw = ''
+        pw_space_error = 'Spaces are not valid'
 
     if has_spaces(e_mail):
-        e_mail_error = 'Spaces are not valid'
-        e_mail = ''
-    elif len(e_mail) > 20 or len(e_mail) < 3:
-        e_mail_error = "password out of range (3-20)"
-        e_mail = ''
-    else:
-        if count_email_sign(e_mail, ".") or count_email_sign(e_mail,"@"):
-            e_mail_error = 'e_mail address must contain a single . and @ symbol.'
-            e_mail = ''
-                     
-    if not user_name_error and not password_error and not e_mail_error:
+        email_space_error = 'Spaces are not valid'
+    if len(e_mail) > 20 or len(e_mail) < 3:
+        email_range_error = "password out of range (3-20)"
+    if count_email_sign(e_mail, ".") or count_email_sign(e_mail,"@"):
+        email_sign_error = 'e_mail address must contain a single . and @ symbol.'
+
+    if pw_range_error or pw_space_error or pw_match_error or email_range_error or email_sign_error or email_space_error or name_range_error or name_space_error:
+        password = ''
+        verify_pw = ''  
+       
+
+         
+    if not name_space_error and not name_range_error and not pw_match_error and not pw_space_error and not pw_range_error and not email_sign_error and not email_range_error and not email_space_error:
         user_name = request.form['user_name']
         return render_template('welcome_user.html', user_name=user_name)
     else:
-        return render_template('index.html', user_name_error=user_name_error, password_error=password_error,
-            e_mail_error = e_mail_error, user_name=user_name, password=password, verify_pw = verify_pw, e_mail = e_mail)
+        return render_template('index.html', name_space_error=name_space_error, name_range_error=name_range_error, pw_space_error= pw_space_error, pw_match_error=pw_match_error, pw_range_error=pw_range_error,
+            email_space_error=email_space_error, email_range_error= email_range_error, email_sign_error= email_sign_error, user_name=user_name, password=password, verify_pw = verify_pw, e_mail = e_mail)
 
 app.run()
